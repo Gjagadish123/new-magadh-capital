bcf3d384646643a50bfaa381398e26e199eaddd7
venugopal <venugopal0161@gmail.com>
2025-08-18 18:18:46 +0530

    feat: :sparkles: backbone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { TeamComponent } from './team/team.component';
 import { BackboneComponent } from './backbone/backbone.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { DifferenceComponent } from './difference/difference.component';
 import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 import { DisclosureComponent } from './disclosure/disclosure.component';
+import { DownloadsComponent } from './downloads/downloads.component';
 import { FaqsComponent } from './faqs/faqs.component';
+import { GrievancesComponent } from './grievances/grievances.component';
+import { HomeComponent } from './home/home.component';
+import { HowToInvestWithUsComponent } from './how-to-invest-with-us/how-to-invest-with-us.component';
 import { InspirationComponent } from './inspiration/inspiration.component';
+import { InvestmentProcessComponent } from './investment-process/investment-process.component';
+import { InvestorFaqComponent } from './investor-faq/investor-faq.component';
+import { LoginInComponent } from './login-in/login-in.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { PhilosophyComponent } from './philosophy/philosophy.component';
 import { PmsfaqComponent } from './pmsfaq/pmsfaq.component';
 import { ResoursesComponent } from './resourses/resourses.component';
 import { RiskManagementComponent } from './risk-management/risk-management.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { InvestmentProcessComponent } from './investment-process/investment-process.component';
-import { HowToInvestWithUsComponent } from './how-to-invest-with-us/how-to-invest-with-us.component';
-import { DownloadsComponent } from './downloads/downloads.component';
-import { GrievancesComponent } from './grievances/grievances.component';
-import { InvestorFaqComponent } from './investor-faq/investor-faq.component';
-import { LoginInComponent } from './login-in/login-in.component';
+import { TeamComponent } from './team/team.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'index', component: HomeComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'team', component: TeamComponent },
   { path: 'backbone', component: BackboneComponent },
